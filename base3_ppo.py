cc9b12f7561dc24552915e8b22c7fb6d71edecf4
aisd <dava0010@algonquinnlive.com>
2025-02-15 02:25:34 -0500

    PPO agent worked

---
+++ base3_ppo.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+import blocksworld_env
+from stable_baselines3 import PPO
+
+env = gym.make("blocksworld_env/BlocksWorld-v1", render_mode="human")
+observation, info = env.reset() 
+
+model = PPO("MultiInputPolicy", env, verbose=1)
+model.learn(total_timesteps=10000, log_interval=4)
+
+for _ in range(10000):
+    #action = env.action_space.sample()  # agent policy that uses the observation and info
+    action, _states = model.predict(observation, deterministic=True)
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
